9fe47b88204ab1cce2f3d7e4cf0ae6d7edeb4d7e
Lucas Falcão <152937620+lucasF4lcao@users.noreply.github.com>
2025-06-03 12:11:16 -0300

    Update front.clj

--- src/fit/front.clj
+++ src/fit/front.clj
@@ -3,150 +3,156 @@
             [clojure.data.json :as json]
             [clojure.string :as str]))
 
-(def base-url "http://localhost:3000")
+(def baseUrl "http://localhost:3000")
 
-(def sessao (atom nil)) ; guarda {:id "usuario123"}
+(def sessao (atom nil))
 
-(defn ler-string [prompt]
+(defn lerString [prompt]
   (println prompt)
   (flush)
   (read-line))
 
-(defn ler-int [prompt]
-  (Integer/parseInt (ler-string prompt)))
+(defn lerInt [prompt]
+  (Integer/parseInt (lerString prompt)))
 
-(defn ler-float [prompt]
-  (Double/parseDouble (ler-string prompt)))
+(defn lerFloat [prompt]
+  (Double/parseDouble (lerString prompt)))
 
-;; ----------- AUTENTICAÇÃO -----------
 
-(defn cadastrar-usuario []
-  (let [id (ler-string "ID do usuário:")
-        senha (ler-string "Senha:")
-        altura (ler-float "Altura (m):")
-        peso (ler-float "Peso (kg):")
-        idade (ler-int "Idade:")
-        sexo (ler-string "Sexo (M/F):")]
+(defn cadastrarUsuario []
+  (let [id (lerString "ID do usuario:")
+        senha (lerString "Senha:")
+        altura (lerFloat "Altura (m):")
+        peso (lerFloat "Peso (kg):")
+        idade (lerInt "Idade:")
+        sexo (lerString "Sexo (m/f):")]
     (let [usuario {:id id
                    :senha senha
                    :altura altura
                    :peso peso
                    :idade idade
                    :sexo sexo}
-          resposta (http/post (str base-url "/usuario")
+          resposta (http/post (str baseUrl "/usuario")
                               {:body (json/write-str usuario)
                                :headers {"Content-Type" "application/json"}
                                :as :json})]
       (println "Resposta:" (:body resposta)))))
 
 (defn login []
-  (let [id (ler-string "ID:")
-        senha (ler-string "Senha:")
+  (let [id (lerString "ID:")
+        senha (lerString "Senha:")
         credenciais {:id id :senha senha}
-        resposta (http/post (str base-url "/login")
+        resposta (http/post (str baseUrl "/login")
                             {:body (json/write-str credenciais)
                              :headers {"Content-Type" "application/json"}
                              :throw-exceptions false
                              :as :json})]
     (if (= 200 (:status resposta))
       (do (reset! sessao {:id id})
-          (println "Login bem-sucedido!")
+          (println "Login feito com sucesso")
           true)
       (do (println "Falha no login:" (:body resposta))
           false))))
 
-(defn consultar-usuario []
+(defn consultarUsuario []
   (let [id (:id @sessao)
-        resposta (http/get (str base-url "/usuario/" id) {:as :json})]
-    (println "Usuário atual:" (:body resposta))))
+        resposta (http/get (str baseUrl "/usuario/" id) {:as :json})]
+    (println "Usuario atual:" (:body resposta))))
 
-;; ----------- FUNCIONALIDADES -----------
 
-(defn registrar-alimento []
-  (let [nome (ler-string "Nome do alimento:")
-        calorias (ler-int "Calorias:")
-        data (ler-string "Data (yyyy-MM-dd) [enter para hoje]:")]
+(defn registrarAlimento []
+  (let [nome (lerString "Nome do alimento:")
+        calorias (lerInt "Calorias:")
+        data (lerString "Data (yyyy-MM-dd) [enter para hoje]:")]
     (let [alimento (cond-> {:nome nome :calorias calorias}
                            (not (str/blank? data)) (assoc :data data))
-          resposta (http/post (str base-url "/alimento")
+          resposta (http/post (str baseUrl "/alimento")
                               {:body (json/write-str alimento)
                                :headers {"Content-Type" "application/json"}
                                :as :json})]
       (println "Resposta:" (:body resposta)))))
 
-(defn registrar-exercicio []
-  (let [nome (ler-string "Nome do exercício:")
-        calorias (ler-int "Calorias gastas:")
-        data (ler-string "Data (yyyy-MM-dd) [enter para hoje]:")]
+(defn registrarExercicio []
+  (let [nome (lerString "Nome do exercicio:")
+        calorias (lerInt "Calorias gastas:")
+        data (lerString "Data (yyyy-MM-dd) [enter para hoje]:")]
     (let [exercicio (cond-> {:nome nome :calorias calorias}
                             (not (str/blank? data)) (assoc :data data))
-          resposta (http/post (str base-url "/exercicio")
+          resposta (http/post (str baseUrl "/exercicio")
                               {:body (json/write-str exercicio)
                                :headers {"Content-Type" "application/json"}
                                :as :json})]
       (println "Resposta:" (:body resposta)))))
 
-(defn consultar-extrato []
-  (let [data-inicio (ler-string "Data início (yyyy-MM-dd) [enter para ignorar]:")
-        data-fim (ler-string "Data fim (yyyy-MM-dd) [enter para ignorar]:")
-        query (->> [["data-inicio" data-inicio]
-                    ["data-fim" data-fim]]
+(defn consultarExtrato []
+  (let [dataInicio (lerString "Data inicio (yyyy-MM-dd) [enter para ignorar]:")
+        dataFim (lerString "Data fim (yyyy-MM-dd) [enter para ignorar]:")
+        query (->> [["data-inicio" dataInicio]
+                    ["data-fim" dataFim]]
                    (filter (fn [[_ v]] (not (str/blank? v))))
                    (map (fn [[k v]] (str k "=" v)))
                    (str/join "&"))
-        url (str base-url "/extrato" (when (seq query) (str "?" query)))
+        url (str baseUrl "/extrato" (when (seq query) (str "?" query)))
         resposta (http/get url {:as :json})]
     (println "Extrato:" (:body resposta))))
 
-(defn consultar-saldo []
-  (let [data-inicio (ler-string "Data início (yyyy-MM-dd) [enter para ignorar]:")
-        data-fim (ler-string "Data fim (yyyy-MM-dd) [enter para ignorar]:")
-        query (->> [["data-inicio" data-inicio]
-                    ["data-fim" data-fim]]
+(defn consultarSaldo []
+  (let [dataInicio (lerString "Data inicio (yyyy-MM-dd) [enter para ignorar]:")
+        dataFim (lerString "Data fim (yyyy-MM-dd) [enter para ignorar]:")
+        query (->> [["data-inicio" dataInicio]
+                    ["data-fim" dataFim]]
                    (filter (fn [[_ v]] (not (str/blank? v))))
                    (map (fn [[k v]] (str k "=" v)))
                    (str/join "&"))
-        url (str base-url "/saldo" (when (seq query) (str "?" query)))
+        url (str baseUrl "/saldo" (when (seq query) (str "?" query)))
         resposta (http/get url {:as :json})]
     (println "Saldo de calorias:" (:saldo (:body resposta)))))
 
-;; ----------- MENU -----------
+(defn haUsuarioRegistrado? []
+  (let [resposta (http/get (str baseUrl "/debug/state") {:as :json})
+        usuarios (:usuarios (:body resposta))]
+    (not (empty? usuarios))))
+
 
 (defn menu []
   (loop []
     (println "\n--- Menu Fit CLI ---")
-    (println "1 - Consultar usuário")
+    (println "1 - Consultar usuario")
     (println "2 - Registrar alimento")
-    (println "3 - Registrar exercício")
+    (println "3 - Registrar exercicio")
     (println "4 - Consultar extrato")
     (println "5 - Consultar saldo")
     (println "0 - Sair")
-    (print "Escolha uma opção: ")
+    (print "Escolha uma opcao: ")
     (flush)
     (case (read-line)
-      "1" (do (consultar-usuario) (recur))
-      "2" (do (registrar-alimento) (recur))
-      "3" (do (registrar-exercicio) (recur))
-      "4" (do (consultar-extrato) (recur))
-      "5" (do (consultar-saldo) (recur))
+      "1" (do (consultarUsuario) (recur))
+      "2" (do (registrarAlimento) (recur))
+      "3" (do (registrarExercicio) (recur))
+      "4" (do (consultarExtrato) (recur))
+      "5" (do (consultarSaldo) (recur))
       "0" (println "Tchau!")
-      (do (println "Opção inválida.") (recur)))))
+      (do (println "Opcao invalida.") (recur)))))
 
-(defn tela-inicial []
-  (loop []
-    (println "\n--- Bem-vindo ao Fit CLI ---")
-    (println "1 - Login")
-    (println "2 - Cadastrar novo usuário")
-    (println "0 - Sair")
-    (print "Escolha uma opção: ")
-    (flush)
-    (case (read-line)
-      "1" (if (login)
-            (menu)
-            (recur))
-      "2" (do (cadastrar-usuario) (recur))
-      "0" (println "Tchau!")
-      (do (println "Opção inválida.") (recur)))))
+(defn telaInicial []
+  (if (haUsuarioRegistrado?)
+    (loop []
+      (println "\n--- Bem-vindo ao Fit CLI ---")
+      (println "1 - Login")
+      (println "0 - Sair")
+      (print "Escolha uma opcao: ")
+      (flush)
+      (case (read-line)
+        "1" (if (login)
+              (menu)
+              (recur))
+        "0" (println "Tchau!")
+        (do (println "Opcao invalida.") (recur))))
+    (do
+      (println "\n--- Nenhum usuario registrado ---")
+      (cadastrarUsuario)
+      (println "Agora faca login...")
+      (telaInicial))))
 
 (defn -main []
-  (tela-inicial))
+  (telaInicial))
